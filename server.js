cd79710327393e628449eb42982c6e4b5052cb38
thirumurugan27 <thirumurugan0200702gmail.com>
2025-11-04 10:03:51 +0530

    initial commit

--- server.js
+++ server.js
@@ -12,8 +12,9 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Thiru+2007',
-  database: 'sample'
+  password: 'your password',
+  database: 'your schema name',
+  dialect: 'mysql'
 });
 
 db.connect(err => {
